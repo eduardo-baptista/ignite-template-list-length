f452a1cc2b57ab498e903435c76e48220776e522
Eduardo Baptista <eduardo.baptista@vulcanet.com.br>
2021-05-04 19:11:42 -0300

    Add method to calculate list lenght

--- lib/list_lenght.ex
+++ lib/list_lenght.ex
@@ -1,5 +1,8 @@
 defmodule ListLenght do
-  def call() do
-    #TO DO
-  end
+  @spec call(list(number())) :: number()
+  def call(list), do: get_length(list, 0)
+
+  @spec get_length(list(number()), number()) :: number()
+  defp get_length([], acc), do: acc
+  defp get_length([_head | tail], acc), do: get_length(tail, acc + 1)
 end
